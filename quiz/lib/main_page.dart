49f16ce05ee65ee1904b55a83f63c855b0021a3d
Hoteldusk <Hoteldusk>
2023-09-19 23:46:38 +0900

    프로토 타입 ver2

--- quiz/lib/main_page.dart
+++ quiz/lib/main_page.dart
@@ -98,91 +98,93 @@ class _MainPageState extends State<MainPage> {
               ]),
         ),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
-                child: const Text(
-                  "모두의 퀴즈",
-                  style: TextStyle(
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue),
-                ),
-              ),
-              //  퀴즈 풀기 버튼
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
-                width: 350,
-                height: 70,
-                child: TextButton(
-                  onPressed: () async {
-                    await getData();
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SelectQuiz(
-                            listSize: result.size,
-                            quizList: result.docs,
-                          ),
-                        ));
-                  },
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
                   child: const Text(
-                    "퀴즈 풀기",
-                    style: TextStyle(fontSize: 30),
+                    "모두의 퀴즈",
+                    style: TextStyle(
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue),
                   ),
                 ),
-              ),
-              //  퀴즈 등록 버튼
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                width: 350,
-                height: 70,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => InsertQuiz(
-                            addData: addData,
-                          ),
-                        ));
-                  },
-                  child: const Text(
-                    "퀴즈 등록",
-                    style: TextStyle(fontSize: 30),
+                //  퀴즈 풀기 버튼
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+                  width: 350,
+                  height: 70,
+                  child: TextButton(
+                    onPressed: () async {
+                      await getData();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SelectQuiz(
+                              listSize: result.size,
+                              quizList: result.docs,
+                            ),
+                          ));
+                    },
+                    child: const Text(
+                      "퀴즈 풀기",
+                      style: TextStyle(fontSize: 30),
+                    ),
                   ),
                 ),
-              ),
-              //  퀴즈 확인 버튼
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                width: 350,
-                height: 70,
-                child: TextButton(
-                  onPressed: () async {
-                    await getData();
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ManageQuiz(
-                            result: result,
-                            quizList: result.docs,
-                            listSize: result.size,
-                            updateData: updateData,
-                            getData: getData,
-                            deleteData: deleteData,
-                          ),
-                        ));
-                  },
-                  child: const Text(
-                    "퀴즈 목록",
-                    style: TextStyle(fontSize: 30),
+                //  퀴즈 등록 버튼
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                  width: 350,
+                  height: 70,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => InsertQuiz(
+                              addData: addData,
+                            ),
+                          ));
+                    },
+                    child: const Text(
+                      "퀴즈 등록",
+                      style: TextStyle(fontSize: 30),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                //  퀴즈 확인 버튼
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                  width: 350,
+                  height: 70,
+                  child: TextButton(
+                    onPressed: () async {
+                      await getData();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ManageQuiz(
+                              result: result,
+                              quizList: result.docs,
+                              listSize: result.size,
+                              updateData: updateData,
+                              getData: getData,
+                              deleteData: deleteData,
+                            ),
+                          ));
+                    },
+                    child: const Text(
+                      "퀴즈 목록",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
